66f8e4aa66b6bbc7a7d3fa045d0b983be6f28295
Gabriel <44093861+GaboAlex@users.noreply.github.com>
2019-01-17 17:01:21 -0500

    Add files via upload

---
+++ Domain/IRepositories/IGrupoRepository.cs
@@ -0,0 +1,13 @@
+﻿//using CodiJobService.Model.CodiJobDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Domain.IRepositories
+{
+    public interface IGrupoRepository : IRepository<TGrupo>
+    {
+    }
+}
---
+++ Domain/IRepositories/IProyectoRepository.cs
@@ -0,0 +1,14 @@
+﻿//using CodiJobService.Model.CodiJobDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Domain.IRepositories
+{
+    public interface IProyectoRepository : IRepository<TProyecto>
+    {
+        IQueryable<TProyecto> FilterProyectos(int pageSize, int page);
+    }
+}
